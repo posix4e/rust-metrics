306ab7afd68ebbbb8ad4e6fc4af809c8e612b2a0
Endre Karlson <endre.karlson@hp.com>
2014-10-17 23:41:47 +0200

    Make Gauge more general and remove main

--- src/gauge.rs
+++ src/gauge.rs
@@ -1,29 +1,31 @@
-pub struct Gauge {
-    pub value: i64
+pub struct Gauge<T> {
+    pub value: T
 }
 
-impl Gauge {
-    pub fn update(&mut self, value: i64) {
-        self.value = value;
+
+impl<T> Gauge<T> {
+    pub fn update(&mut self, value: T) {
+        self.value = value
     }
 
-    pub fn snapshot(&self) -> Gauge {
+    pub fn snapshot(self) -> Gauge<T> {
         Gauge { value: self.value }
     }
 }
+
 
 #[cfg(test)]
 mod test {
     use gauge::Gauge;
 
     #[test]
-    fn create_and_shnapshot() {
-        let g = Gauge{value: 0};
-
+    fn create_and_snapshot() {
+        let g: Gauge<f64> = Gauge {value: 0f64 };
         let mut g_snapshot = g.snapshot();
-        g_snapshot.update(10);
 
-        assert!(g.value == 0);
-        assert!(g_snapshot.value == 10);
+        g_snapshot.update(10f64);
+
+        assert!(g.value == 0f64);
+        assert!(g_snapshot.value == 10f64);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![crate_name = "metrics"]
 
+#![feature(if_let)]
+
 extern crate time;
 
 #[cfg(test)] extern crate test;
 pub mod counter;
 pub mod gauge;
-pub mod gauge_f64;
 pub mod ewma;
 pub mod meter;
 pub mod metric;
