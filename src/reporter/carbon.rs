18469245eae6423a3c1e275402bcf2375303fa6b
Bruce Mitchener <bruce.mitchener@gmail.com>
2016-06-23 14:45:50 +0700

    Add license header to every Rust file.

--- src/reporter/carbon.rs
+++ src/reporter/carbon.rs
@@ -1,3 +1,9 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 // CarbonReporter sends a message to a carbon end point at a regular basis.
 use registry::{Registry, StdRegistry};
 use std::time::Duration;
