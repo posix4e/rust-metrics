15c3d7cead43377a33924e6fed69532451100418
Alex Newman <posix4e@gmail.com>
2016-07-10 20:44:07 -0700

    Rust fmt

--- examples/web_server_with_debug_port.rs
+++ examples/web_server_with_debug_port.rs
@@ -19,43 +19,43 @@ use std::collections::HashMap;
 use histogram::Histogram;
 
 #[cfg(not(feature = "prometheus"))]
-fn main () { }
+fn main() {}
 #[cfg(feature = "prometheus")]
 fn main() {
     use metrics::reporter::PrometheusReporter;
-        println!("WebServer Starting");
-        extern crate hyper;
-        let m = StdMeter::new();
-        m.mark(100);
+    println!("WebServer Starting");
+    extern crate hyper;
+    let m = StdMeter::new();
+    m.mark(100);
 
-        let c = StdCounter::new();
-        c.inc();
+    let c = StdCounter::new();
+    c.inc();
 
-        let g = StdGauge::new();
-        g.set(32);
+    let g = StdGauge::new();
+    g.set(32);
 
-        let mut h = Histogram::configure()
-            .max_value(100)
-            .precision(1)
-            .build()
-            .unwrap();
+    let mut h = Histogram::configure()
+        .max_value(100)
+        .precision(1)
+        .build()
+        .unwrap();
 
-        h.increment_by(1, 1).unwrap();
+    h.increment_by(1, 1).unwrap();
 
-        let mut labels = HashMap::new();
-        labels.insert(String::from("test"), String::from("test"));
-        let mut r = StdRegistry::new_with_labels(labels);
-        r.insert("meter1", Metric::Meter(m.clone()));
-        r.insert("counter1", Metric::Counter(c.clone()));
-        r.insert("gauge1", Metric::Gauge(g.clone()));
-        r.insert("histogram", Metric::Histogram(h));
+    let mut labels = HashMap::new();
+    labels.insert(String::from("test"), String::from("test"));
+    let mut r = StdRegistry::new_with_labels(labels);
+    r.insert("meter1", Metric::Meter(m.clone()));
+    r.insert("counter1", Metric::Counter(c.clone()));
+    r.insert("gauge1", Metric::Gauge(g.clone()));
+    r.insert("histogram", Metric::Histogram(h));
 
-        let arc_registry = Arc::new(r);
-        let reporter =
-            PrometheusReporter::new(arc_registry.clone(), "test", "0.0.0.0:9090", "asd.asdf");
-        reporter.start();
-        Iron::new(|_: &mut Request| Ok(Response::with(status::NotFound)))
-            .http("0.0.0.0:3000")
-            .unwrap();
-        println!("WebServer Running");
+    let arc_registry = Arc::new(r);
+    let reporter =
+        PrometheusReporter::new(arc_registry.clone(), "test", "0.0.0.0:9090", "asd.asdf");
+    reporter.start();
+    Iron::new(|_: &mut Request| Ok(Response::with(status::NotFound)))
+        .http("0.0.0.0:3000")
+        .unwrap();
+    println!("WebServer Running");
 }
